97ba75c878f48929403d35985f691844e8e3ccaf
전승리 <jws08219@naver.com>
2022-09-20 23:17:15 +0900

    VersionManage

--- springRestAPI/src/main/java/com/example/springRestAPI/user/AdminUserController.java
+++ springRestAPI/src/main/java/com/example/springRestAPI/user/AdminUserController.java
@@ -3,24 +3,22 @@ package com.example.springRestAPI.user;
 import com.fasterxml.jackson.databind.ser.FilterProvider;
 import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
 import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
-import com.fasterxml.jackson.databind.util.BeanUtil;
 import org.springframework.beans.BeanUtils;
-import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import javax.validation.Valid;
-import java.net.URI;
 import java.util.List;
+/*
+    버전관리는 URI,PARAMETER, MIME TYPE, HEADER VERSION 이있는데 일반 브라우저에서 사용하는지 여부를 고려해 사용해야한다.
+ */
 
 @RestController
 @RequestMapping("/admin")
-public class AdminController {
+public class AdminUserController {
 
     private UserDaoService service;
 
-    public AdminController(UserDaoService service) {
+    public AdminUserController(UserDaoService service) {
         this.service = service;
     }
 
@@ -44,6 +42,7 @@ public class AdminController {
         return mapping;
     }
 
+    /*
     @GetMapping("/v1/users/{id}")
     public MappingJacksonValue retrieveUserV1(@PathVariable(name = "id") int id) {
         User user = service.findOne(id);
@@ -67,6 +66,68 @@ public class AdminController {
 
     @GetMapping("/v2/users/{id}")
     public MappingJacksonValue retrieveUserV2(@PathVariable(name = "id") int id) {
+        User user = service.findOne(id);
+        if (user == null) {
+            throw new UserNotFoundException(String.format("ID[%s] not found", id));
+        }
+
+        UserV2 userV2 = new UserV2();
+        //객체 변환
+        BeanUtils.copyProperties(user, userV2);
+        userV2.setGrade("VIP");
+
+        SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter
+                .filterOutAllExcept("id", "name", "joinDate", "grade");
+
+
+        // User 엔티티에 json filter 걸어준 부분
+        FilterProvider filters = new SimpleFilterProvider().addFilter("UserInfoV2", filter);
+
+        MappingJacksonValue mapping = new MappingJacksonValue(userV2);
+
+        mapping.setFilters(filters);
+
+        return mapping;
+    }
+     */
+
+//  params로 버전관리
+//    @GetMapping(value = "/users/{id}/", params = "version=1")
+//  헤더로 버전관리
+//    @GetMapping(value = "/users/{id}", headers ="X-API-VERSION=1")
+//    MIME TYPE 이용 방법
+    @GetMapping(value = "/users/{id}", produces = "application/vnd.company.appv1+json")
+    public MappingJacksonValue retrieveUserV1(@PathVariable int id) {
+        User user = service.findOne(id);
+        if (user == null) {
+            throw new UserNotFoundException(String.format("ID[%s] not found", id));
+        }
+
+        SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter
+                .filterOutAllExcept("id", "name", "joinDate", "ssn");
+
+
+        // User 엔티티에 json filter 걸어준 부분
+        FilterProvider filters = new SimpleFilterProvider().addFilter("UserInfo", filter);
+
+        MappingJacksonValue mapping = new MappingJacksonValue(user);
+
+        mapping.setFilters(filters);
+
+        return mapping;
+    }
+
+
+
+//  params로 버전관리
+//    @GetMapping(value = "/users/{id}/", params = "version=2")
+
+//  헤더로 버전관리
+//  @GetMapping(value = "/users/{id}", headers ="X-API-VERSION=2")
+
+//    MIME TYPE 이용 방법 HEADER에 ACCETP 키에 produces 값을 넣는다.
+@GetMapping(value = "/users/{id}", produces = "application/vnd.company.appv2+json")
+public MappingJacksonValue retrieveUserV2(@PathVariable(name = "id") int id) {
         User user = service.findOne(id);
         if (user == null) {
             throw new UserNotFoundException(String.format("ID[%s] not found", id));
