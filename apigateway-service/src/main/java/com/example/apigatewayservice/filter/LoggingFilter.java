807f2168f4e063b09141685c52bb306ac5cb0b8d
전승리 <jws08219@naver.com>
2022-10-01 16:54:15 +0900

    LoggerFilter

--- apigateway-service/src/main/java/com/example/apigatewayservice/filter/LoggingFilter.java
+++ apigateway-service/src/main/java/com/example/apigatewayservice/filter/LoggingFilter.java
@@ -3,7 +3,9 @@ package com.example.apigatewayservice.filter;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.OrderedGatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.core.Ordered;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
@@ -21,22 +23,26 @@ public class LoggingFilter extends AbstractGatewayFilterFactory<LoggingFilter.Co
     @Override
     public GatewayFilter apply(Config config) {
         // Custom pre Filter
-        return (exchange, chain) -> {
+//        return (exchange, chain) -> {
+
+
+        GatewayFilter filter = new OrderedGatewayFilter((exchange, chain) -> {
             ServerHttpRequest request = exchange.getRequest();
             ServerHttpResponse response = exchange.getResponse();
 
-            log.info("Global filter baseMessage : {}", config.getBaseMessage());
+            log.info("Logging filter baseMessage : {}", config.getBaseMessage());
 
             if (config.isPreLogger()) {
-                log.info("Global Filter Start: request id => {}", request.getId());
+                log.info("Logging PRE Filter Start: request id => {}", request.getId());
             }
             // Global Post filter
             return chain.filter(exchange).then(Mono.fromRunnable(() -> {
                 if (config.isPostLogger()) {
-                    log.info("Global Filter End : response code -> {}", response.getStatusCode());
+                    log.info("Logging POST Filter End : response code -> {}", response.getStatusCode());
                 }
             }));
-        };
+        }, Ordered.LOWEST_PRECEDENCE /* 필터 우선순위 */ );
+        return filter;
     }
 
     @Data
