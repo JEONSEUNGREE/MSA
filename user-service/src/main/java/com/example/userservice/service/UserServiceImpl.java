50559becdf8c29e11e72c1412186b5e9aa867364
전승리 <jws08219@naver.com>
2022-10-05 23:37:10 +0900

    userService_1

---
+++ user-service/src/main/java/com/example/userservice/service/UserServiceImpl.java
@@ -0,0 +1,10 @@
+package com.example.userservice.service;
+
+import com.example.userservice.dto.UserDto;
+
+public class UserServiceImpl implements UserService {
+    @Override
+    public UserDto createUser(UserDto userDto) {
+        return null;
+    }
+}
